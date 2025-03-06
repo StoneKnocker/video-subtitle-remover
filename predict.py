64f6fc3570880389fb0d12a6e8c1def0adc76b6d
xianyang <xianyang.lu@vcg.com>
2025-03-06 16:04:27 +0800

    save predict

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
 # Prediction interface for Cog ⚙️
 # https://cog.run/python
 
+from backend.main import SubtitleRemover
 from cog import BasePredictor, Input, Path
+import os
 
 
 class Predictor(BasePredictor):
@@ -11,12 +13,12 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
+        video_path: Path = Input(description="input the video file"),
     ) -> Path:
         """Run a single prediction on the model"""
         # processed_input = preprocess(image)
         # output = self.model(processed_image, scale)
         # return postprocess(output)
+        sd = SubtitleRemover(video_path, sub_area=None)
+        sd.run()
+        return f"{os.path.basename(video_path).rsplit('.', 1)[0]}_no_sub.mp4"
