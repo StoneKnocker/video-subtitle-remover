47cea6057389e952799874245cb5b3abe9e31be0
StoneKnocker <emluxy@gmail.com>
2025-05-06 09:51:28 +0800

    Revert "debug"
    This reverts commit ed1a7ace8570279d1898d682c66238a2ac27faba.

--- backend/tools/infer/utility.py
+++ backend/tools/infer/utility.py
@@ -24,7 +24,6 @@ import math
 from paddle import inference
 import time
 from ppocr.utils.logging import get_logger
-import backend.config as config
 
 
 def str2bool(v):
@@ -44,7 +43,7 @@ def init_args():
     # params for text detector
     parser.add_argument("--image_dir", type=str)
     parser.add_argument("--det_algorithm", type=str, default='DB')
-    parser.add_argument("--det_model_dir", type=str, default=config.DET_MODEL_PATH)
+    parser.add_argument("--det_model_dir", type=str)
     parser.add_argument("--det_limit_side_len", type=float, default=960)
     parser.add_argument("--det_limit_type", type=str, default='max')
 
